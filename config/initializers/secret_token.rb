033c43daa2064f4aab658720d44d60b9e455de00
Brian Joseff <Brian.D.P.Joseff@dartmouth.edu>
2011-06-24 13:53:22 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pazego::Application.config.secret_token = '791e6ba6186b6dc282285739e146f447867f0659da4bc995e560b488a87208cc03381332829cf6f372cd8c6775fa3c4ab7fce9992b13d5c5d4b61bd9507e793c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Pazego::Application.config.session_store :cookie_store, :key => '_pazego_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Pazego::Application.config.session_store :active_record_store
